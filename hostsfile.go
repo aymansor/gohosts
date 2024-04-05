cbcbaa51f157c469e4e9aae8ed1b82036bbd31f4
aymansor <almansorayman@gmail.com>
2024-04-05 01:53:23 -0400

    feat: modify Save method to write directly to hosts file and restore backup on error

--- hostsfile.go
+++ hostsfile.go
@@ -81,20 +81,23 @@ func (h *HostsFile) Save() error {
 		return fmt.Errorf("failed to create backup: %v", err)
 	}
 
-	// Create a temporary file to write the modified hosts file instead of modifying the original file
-	// directly. This is done to prevent data loss in case of an error while writing the file.
-	tempFile, err := os.CreateTemp("", "hosts-")
+	// Open the hosts file for writing
+	file, err := os.OpenFile(h.path, os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
-		return fmt.Errorf("failed to create temporary file: %v", err)
+		return fmt.Errorf("failed to open hosts file: %v", err)
 	}
-	defer os.Remove(tempFile.Name())
+	defer file.Close()
 
-	// Start by writing the additional content to the temporary file
+	// Start by writing the additional content to the file
 	// This is the content that was not parsed as host entries
-	writer := bufio.NewWriter(tempFile)
+	writer := bufio.NewWriter(file)
 	_, err = writer.WriteString(h.AditionalContent)
 	if err != nil {
-		return fmt.Errorf("failed to write additional content to temporary file: %v", err)
+		// If an error occurs while writing, restore the backup
+		if restoreErr := h.RestoreBackup(); restoreErr != nil {
+			return fmt.Errorf("failed to write additional content to hosts file: %v, and failed to restore backup: %v", err, restoreErr)
+		}
+		return fmt.Errorf("failed to write additional content to hosts file: %v", err)
 	}
 
 	for _, entry := range h.Entries {
@@ -107,22 +110,24 @@ func (h *HostsFile) Save() error {
 		if !entry.Active {
 			line = "# " + line
 		}
-		// Then write the host entry to the temporary file
+		// Then write the host entry to the file
 		_, err := writer.WriteString(line + "\n")
 		if err != nil {
-			return fmt.Errorf("failed to write entry to temporary file: %v", err)
+			// If an error occurs while writing, restore the backup
+			if restoreErr := h.RestoreBackup(); restoreErr != nil {
+				return fmt.Errorf("failed to write entry to hosts file: %v, and failed to restore backup: %v", err, restoreErr)
+			}
+			return fmt.Errorf("failed to write entry to hosts file: %v", err)
 		}
 	}
 
 	err = writer.Flush()
 	if err != nil {
+		// If an error occurs while flushing, restore the backup
+		if restoreErr := h.RestoreBackup(); restoreErr != nil {
+			return fmt.Errorf("failed to flush writer: %v, and failed to restore backup: %v", err, restoreErr)
+		}
 		return fmt.Errorf("failed to flush writer: %v", err)
-	}
-
-	// Finally replace the original hosts file with the temporary file
-	err = os.Rename(tempFile.Name(), h.path)
-	if err != nil {
-		return fmt.Errorf("failed to replace hosts file: %v", err)
 	}
 
 	return nil
